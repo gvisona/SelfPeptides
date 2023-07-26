3184feec3ebd2899297c96a7ad9282eeff5ce3e5
Giovanni Visona <giovanni.visona@tuebingen.mpg.de>
2023-07-26 10:02:54 +0200

    classifier sweep

--- hyperparameter_sweeps/Classifier_sweep.py
+++ hyperparameter_sweeps/Classifier_sweep.py
@@ -17,7 +17,7 @@ if __name__=="__main__":
     parser.add_argument("--seed", type=int, default=0)
     args = parser.parse_args()
     
-    sweep_id = '8pq7nowg'
+    sweep_id = 'zp8tqkef'
     
     if os.path.exists("/home/gvisona/Projects/Immunology"):
         project_folder = "/home/gvisona/Projects/Immunology"
--- jobfiles/job_sweep.sh
+++ jobfiles/job_sweep.sh
@@ -6,26 +6,6 @@ module load cudnn/8.6.0-cu11.7
 module load cuda/11.7
 
 
-# python3 /home/gvisona/Immunology/hyperparameter_sweeps/Classifier_fromBApretrained_sweep.py  \
-# python3 /home/gvisona/Immunology/hyperparameter_sweeps/BetaRegr_fromBApretrained_no_class_sweep.py  \
-# python3 /home/gvisona/Immunology/hyperparameter_sweeps/BindingAffinity_sweep.py  \
-python3 /home/gvisona/Immunology/hyperparameter_sweeps/BetaRegr_fromBApretrained_sweep.py  \
+
+python3 /home/gvisona/Immunology/hyperparameter_sweeps/Classifier_sweep.py  \
 --seed $1 \
-
-# --experiment_name "test_metrics_sweep" \
-# --experiment_group "IEDB_CLIPT5_sweeps" \
-# --project_folder "/fast/gvisona/Immunology" \
-# --immunogenicity_df "/home/gvisona/Immunology/processed_data/DHLAP_immunogenicity_data.csv" \
-# --binding_affinity_df "/home/gvisona/Immunology/processed_data/DHLAP_binding_affinity_data.csv" \
-# --pseudo_seq_file "/home/gvisona/Immunology/data/NetMHCpan_pseudoseq/MHC_pseudo.dat" \
-
-# CODE=$?
-# if [ $CODE -eq 0 ]
-# then
-# curl -d "Job N.$1 finished successfully." ntfy.sh/gv_cluster_experiments
-# elif [ $CODE -eq 3 ]
-# then
-# curl -d "Job N.$1 restarted." ntfy.sh/gv_cluster_experiments
-# else
-# curl -d "Job N.$1 failed with exit code $CODE." ntfy.sh/gv_cluster_experiments
-# fi
