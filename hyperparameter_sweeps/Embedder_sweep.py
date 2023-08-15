13dc26eed184b121c8684d0f78c1645633e413cc
Giovanni Visona <giovanni.visona@tuebingen.mpg.de>
2023-08-15 14:44:48 +0200

    new sweep

--- hyperparameter_sweeps/Embedder_sweep.py
+++ hyperparameter_sweeps/Embedder_sweep.py
@@ -18,7 +18,7 @@ if __name__=="__main__":
     parser.add_argument("--seed", type=int, default=0)
     args = parser.parse_args()
     
-    sweep_id = 'r7w6e2w6'
+    sweep_id = 'n7bsc34p'
     
     if os.path.exists("/home/gvisona/Projects/SelfPeptides"):
         project_folder = "/home/gvisona/Projects/SelfPeptides"
