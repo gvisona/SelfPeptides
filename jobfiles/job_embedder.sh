9d25e049d4d0fccc4e9c8571f5b83ba14afd181f
Giovanni Visona <giovanni.visona@tuebingen.mpg.de>
2023-08-16 17:55:20 +0200

    jobfile

--- jobfiles/job_embedder.sh
+++ jobfiles/job_embedder.sh
@@ -5,15 +5,16 @@ source /home/gvisona/environments/prot/bin/activate
 module load cudnn/8.6.0-cu11.7
 module load cuda/11.7
 
-idx=$1+1
+idx=$1
 
 learning_rates=(0.00003 0.00048 0.00192 0.00003 0.00048 0.00192)
 weight_decays=(0.000001 0.000001 0.000001 0.00001 0.00001 0.00001)
 acc_batches=(1 16 64 1 16 64)
 
-lr=${learning_rates[idx]}
-wd=${weight_decays[idx]}
-ab=${acc_batches[idx]}
+lr=${learning_rates[@]:${idx}:1}
+wd=${weight_decays[@]:${idx}:1}
+ab=${acc_batches[@]:${idx}:1}
+
 
 
 python3 /home/gvisona/SelfPeptides/training_scripts/train_selfpeptide_embedder_cmt.py  \
@@ -28,7 +29,7 @@ python3 /home/gvisona/SelfPeptides/training_scripts/train_selfpeptide_embedder_c
 --embedding_dim 512 --projection_hidden_dim 2048 --projection_dim 32 \
 --PMA_num_heads 1 --PMA_ln --num_heads 4 --transf_hidden_dim 2048 --n_attention_layers 2 \
 --batch_size 32 \
---experiment_group "SP_Embedder" --experiment_name "Embeddings_CMT_long" \
+--experiment_group "SP_Embedder" --experiment_name "Embeddings_CMT_flatLR" \
 --project_folder "/fast/gvisona/SelfPeptides" \
 --hdf5_dataset "/home/gvisona/SelfPeptides/processed_data/Self_nonSelf/pre_tokenized_peptides_dataset.hdf5" \
 --pretrained_aa_embeddings "/home/gvisona/SelfPeptides/processed_data/aa_embeddings/learned_BA_AA_embeddings.npy" \
