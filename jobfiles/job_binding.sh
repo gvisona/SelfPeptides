6968a174978fcdd8d62e0c7c3df06313c43b87cc
Giovanni Visona <giovanni.visona@tuebingen.mpg.de>
2023-09-05 10:12:07 +0200

    job 2

--- jobfiles/job_binding.sh
+++ jobfiles/job_binding.sh
@@ -20,15 +20,15 @@ python3 /home/gvisona/SelfPeptides/training_scripts/train_binding_model.py  \
 --accumulate_batches 64 --lr  "$lr" \
 --weight_decay "$wd" \
 --val_size 0.1 --test_size 0.15 \
---validate_every_n_updates 2 \
---max_updates 10 --dropout_p 0.05 \
+--validate_every_n_updates 32 \
+--max_updates 5000 --dropout_p 0.05 \
 --ramp_up 0.2 --min_frac 0.1 --force_warmup \
 --momentum 0.9 --nesterov_momentum \
 --embedding_dim 512 --mlp_hidden_dim 2048 --mlp_num_layers 2 --output_dim 1 \
 --PMA_num_heads 4 --PMA_ln --num_heads 4 --transf_hidden_dim 512 --n_attention_layers 2 \
 --batch_size 32 \
 --hla_repr "Allele Pseudo-sequence" --early_stopping --patience 200 \
---experiment_group "Binding_model_masking" --experiment_name "BA_tune_devel" \
+--experiment_group "Binding_model_masking" --experiment_name "BA_tune" \
 --project_folder "/fast/gvisona/SelfPeptides" \
 --pretrained_aa_embeddings "/home/gvisona/SelfPeptides/processed_data/aa_embeddings/learned_BA_AA_embeddings.npy" \
 --init_checkpoint "/home/gvisona/SelfPeptides/trained_models/binding_model_OLD/checkpoint.pt" \
