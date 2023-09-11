d813db3dbbbfa7e9b820b6d08fe50ab13d13f9ee
Giovanni Visona <giovanni.visona@tuebingen.mpg.de>
2023-09-11 10:36:10 +0200

    n

--- jobfiles/job_sweep.sh
+++ jobfiles/job_sweep.sh
@@ -9,5 +9,5 @@ module load cuda/11.7
 
 # python3 /home/gvisona/SelfPeptides/hyperparameter_sweeps/BindingAffinity_sweep.py  \
 # python3 /home/gvisona/SelfPeptides/hyperparameter_sweeps/Classifier_sweep.py  \
-python3 /home/gvisona/SelfPeptides/hyperparameter_sweeps/Immuogenicity_sweep.py  \
+python3 /home/gvisona/SelfPeptides/hyperparameter_sweeps/Immunogenicity_sweep.py  \
 --seed $1 \
