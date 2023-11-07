ca8963ff7c0247c71d3f505d333862ea91da1c97
Giovanni Visona <giovanni.visona@tuebingen.mpg.de>
2023-11-07 10:07:14 +0100

    sweep lr

--- hyperparameter_sweeps/BindingAffinity_sweep.py
+++ hyperparameter_sweeps/BindingAffinity_sweep.py
@@ -17,7 +17,7 @@ if __name__=="__main__":
     parser.add_argument("--seed", type=int, default=0)
     args = parser.parse_args()
     
-    sweep_id = 'xuwdt65z'
+    sweep_id = 'utdnqg0i'
     
     if os.path.exists("/home/gvisona/Projects/SelfPeptides"):
         project_folder = "/home/gvisona/Projects/SelfPeptides"
