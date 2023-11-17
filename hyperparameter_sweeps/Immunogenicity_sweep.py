48e1514db171b87cf625fb0fa2e51c7f31619515
Giovanni Visona <giovanni.visona@tuebingen.mpg.de>
2023-11-17 11:18:38 +0100

    long sweep

--- hyperparameter_sweeps/Immunogenicity_sweep.py
+++ hyperparameter_sweeps/Immunogenicity_sweep.py
@@ -20,7 +20,7 @@ if __name__=="__main__":
     parser.add_argument("--seed", type=int, default=0)
     args = parser.parse_args()
     
-    sweep_id = 'r6dwarqs'
+    sweep_id = 'w9066c2f'
     
     if os.path.exists("/home/gvisona/Projects/SelfPeptides"):
         project_folder = "/home/gvisona/Projects/SelfPeptides"
