c0b666d7c7163b43d8b5d6c4054a3f0989ba41b4
Giovanni Visona <giovanni.visona@tuebingen.mpg.de>
2024-01-29 15:46:26 +0100

    sweep 1

--- hyperparameter_sweeps/Finetune_ESM2_sweep.py
+++ hyperparameter_sweeps/Finetune_ESM2_sweep.py
@@ -17,7 +17,7 @@ if __name__=="__main__":
     parser.add_argument("--seed", type=int, default=0)
     args = parser.parse_args()
     
-    sweep_id = 'c5w7en9h'
+    sweep_id = 'bsk6n5ym'
     
     if os.path.exists("/home/gvisona/Projects/SelfPeptides"):
         project_folder = "/home/gvisona/Projects/SelfPeptides"
